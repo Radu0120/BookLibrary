b28f880aa170a4c0ceb7dde5997dd15aca79569b
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-10-12 12:42:57 +0200

    Added book class

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get;set;}
+        public string Author { get;set;}
+        public int PageNumber { get;set;}
+        public string ISBN13 { get;set;}
+
+        public Book()
+        {
+
+        }
+        public Book(string title, string author, int pagenumber, string isbn)
+        {
+            Title = title;
+            Author = author;
+            PageNumber = pagenumber;
+            ISBN13 = isbn;
+        }
+    }
+}
